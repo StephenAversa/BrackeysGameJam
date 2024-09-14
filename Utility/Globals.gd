a60a385e491ab1633372e0fb9f00cae3e5a14388
DESKTOP-T3O0OBT\Steph <levelupgodot@gmail.com>
2024-09-14 03:16:39 -0400

    touches

--- Utility/Globals.gd
+++ Utility/Globals.gd
@@ -5,6 +5,9 @@ var gear_count = 0
 var gear_count_for_body = 5
 var gear_count_for_legs = 10
 var gear_count_for_arms = 15
+var gear_count_health1 = 20
+var gear_count_health2 = 25
+var gear_count_health3 = 30
 
 var conveyor_speed = -100
 
